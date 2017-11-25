27da32db82d403d295f8d69ca065a22536cde646
Gustavo Balieiro Felipe <gbalieriof@gmail.com>
2017-11-24 22:29:58 -0200

    Update settings.py

--- lmsimpacta/lmsimpacta/settings.py
+++ lmsimpacta/lmsimpacta/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'lmsimpacta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': mysqlconndict['Database'],
+        'USER': mysqlconndict['User Id'],
+        'PASSWORD': mysqlconndict['Password'],
+        'HOST': mysqlconndict['Data Source'].split(':')[0],
+        'PORT': mysqlconndict['Data Source'].split(':')[1],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
